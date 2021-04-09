f81b473294c0a95db865decc201b6dcfd6c77b24
dungnt <dungliemtuc@gmail.com>
2021-04-09 21:11:03 +0700

    update giao diện v1

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var app = express()
+const express = require('express');
+const path = require('path')
+
+const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, "views"))
+
 app.get('/', function(req, res) {
-    res.send('hello from simple server :)')
+    res.render('index')
 })
 app.listen(port);
